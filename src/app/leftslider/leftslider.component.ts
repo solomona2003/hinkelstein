6c66b695f59d5040b2bc4b96506153f3d8181fb6
Solomon <solomona2003@yahoo.com>
2018-05-29 11:05:38 +0200

    logout name works

--- src/app/leftslider/leftslider.component.ts
+++ src/app/leftslider/leftslider.component.ts
@@ -1,12 +1,13 @@
 import { AuthService } from './../authentication/auth.service';
-import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import {MatSidenavContainer, MatSidenav, MatNavList, MatIcon } from '@angular/material';
 
 @Component({
   selector: 'app-leftslider',
   templateUrl: './leftslider.component.html',
-  styleUrls: ['./leftslider.component.css']
+  styleUrls: ['./leftslider.component.css'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class LeftsliderComponent implements OnInit {
 
