69632436aa7c61d1f6d3266de73c92365f23b602
Solomon <solomona2003@yahoo.com>
2018-05-30 15:45:22 +0200

    partial complete stream control

--- src/app/capture/streamcontrol/streamcontrol.component.ts
+++ src/app/capture/streamcontrol/streamcontrol.component.ts
@@ -24,7 +24,7 @@ export class StreamcontrolComponent implements OnInit {
   playButtonStatus = 'off';
   stopButtonStatus = 'off';
 
-  pausOn = false;
+  pausOn: boolean = false;
   controlStatus = false;
   constructor(private streamingService: StreamingService) { }
 
@@ -45,91 +45,122 @@ export class StreamcontrolComponent implements OnInit {
 
 
   onRecButtonPressed() {
-    let playStatus;
-    let recStatus;
-    this.streamingService.playButton.subscribe(r => {playStatus = r; });
-    this.streamingService.recButton.subscribe(r => {recStatus = r; });
-   if ( playStatus === 'off') {
-     this.controlStatus = true;
-    this.r2rColor = 'primary';
-    this.streamingService.recButton.next('standby');
-   } else if (playStatus === 'on' && recStatus === 'standby') {
-    this.controlStatus = true;
-     this.streamingService.recButton.next('on');
-     this.r2rColor = 'warn';
-   }  else if (playStatus === 'on' && recStatus === 'off') {
-    this.controlStatus = true;
-    this.streamingService.recButton.next('on');
-    this.r2rColor = 'warn';
-   } else if (playStatus === 'on' && recStatus === 'standby') {
-    this.controlStatus = true;
-    this.streamingService.recButton.next('on');
-    this.r2rColor = 'warn';
-   } else if (playStatus === 'on' && recStatus === 'on') {
-    this.controlStatus = true;
-    this.streamingService.recButton.next('standby');
-    this.r2rColor = 'primary';
-  }
+  let playStatus;
+  let recStatus;
+  let webStatus;
+  let r2rStatus;
+  this.streamingService.r2rButton.subscribe(r => {r2rStatus = r; });
+  this.streamingService.webButton.subscribe(r => {webStatus = r; });
+  this.streamingService.playButton.subscribe(r => {playStatus = r; });
+  this.streamingService.recButton.subscribe(r => {recStatus = r; });
+ if ( playStatus === 'off' && recStatus === 'off') {
+   this.controlStatus = true;
+  this.recColor = 'accent';
+  this.streamingService.recButton.next('standby');
+  this.streamingService.playButton.next('on');
+ } else  if (playStatus === 'on' && recStatus === 'standby') {
+  this.recColor = 'basic';
+   this.streamingService.recButton.next('off');
+ } else  if (playStatus === 'on' && recStatus === 'off') {
+  this.recColor = 'accent';
+  this.streamingService.recButton.next('standby');
+ } else  if (playStatus === 'rec' && recStatus === 'standby') {
+  this.recColor = 'warn';
+  this.streamingService.recButton.next('on');
+ } else  if (playStatus === 'rec' && recStatus === 'off') {
+  this.recColor = 'warn';
+  this.streamingService.recButton.next('on');
+ } else  if (playStatus === 'rec' && recStatus === 'on') {
+  this.recColor = 'accent';
+  this.streamingService.recButton.next('standby');
+ }
+
+
+
+ if (recStatus === 'off' && r2rStatus === 'off' && webStatus === 'off') {
+   this.controlStatus = false;
+   this.streamingService.playButton.next('off');
+ }
   }
 
 
 
   onWebButtonPressed() {
-  let playStatus;
-  let webStatus;
-  this.streamingService.playButton.subscribe(r => {playStatus = r; });
-  this.streamingService.webButton.subscribe(r => {webStatus = r; });
- if ( playStatus === 'off') {
+    let playStatus;
+    let recStatus;
+    let webStatus;
+    let r2rStatus;
+    this.streamingService.r2rButton.subscribe(r => {r2rStatus = r; });
+    this.streamingService.webButton.subscribe(r => {webStatus = r; });
+    this.streamingService.playButton.subscribe(r => {playStatus = r; });
+    this.streamingService.recButton.subscribe(r => {recStatus = r; });
+ if ( playStatus === 'off' && webStatus === 'off') {
    this.controlStatus = true;
-  this.r2rColor = 'primary';
+  this.webColor = 'accent';
   this.streamingService.webButton.next('standby');
- } else if (playStatus === 'on' && webStatus === 'standby') {
-  this.controlStatus = true;
-   this.streamingService.webButton.next('on');
-   this.r2rColor = 'warn';
- }  else if (playStatus === 'on' && webStatus === 'off') {
-  this.controlStatus = true;
-  this.streamingService.webButton.next('on');
-  this.r2rColor = 'warn';
- } else if (playStatus === 'on' && webStatus === 'standby') {
-  this.controlStatus = true;
-  this.streamingService.webButton.next('on');
-  this.r2rColor = 'warn';
- } else if (playStatus === 'on' && webStatus === 'on') {
-  this.controlStatus = true;
+  this.streamingService.playButton.next('on');
+ } else  if (playStatus === 'on' && webStatus === 'standby') {
+  this.webColor = 'basic';
+   this.streamingService.webButton.next('off');
+ } else  if (playStatus === 'on' && webStatus === 'off') {
+  this.webColor = 'accent';
   this.streamingService.webButton.next('standby');
-  this.r2rColor = 'primary';
+ }  else  if (playStatus === 'rec' && webStatus === 'standby') {
+  this.webColor = 'warn';
+  this.streamingService.webButton.next('on');
+ } else  if (playStatus === 'rec' && webStatus === 'off') {
+  this.webColor = 'warn';
+  this.streamingService.webButton.next('on');
+ } else  if (playStatus === 'rec' && webStatus === 'on') {
+  this.webColor = 'accent';
+  this.streamingService.webButton.next('standby');
+ }
+
+
+ if (recStatus === 'off' && r2rStatus === 'off' && webStatus === 'off') {
+  this.controlStatus = false;
+  this.streamingService.playButton.next('off');
 }
   }
 
 
 onR2RButtonPressed() {
   let playStatus;
+  let recStatus;
+  let webStatus;
   let r2rStatus;
-  this.streamingService.playButton.subscribe(r => {playStatus = r; });
   this.streamingService.r2rButton.subscribe(r => {r2rStatus = r; });
- if ( playStatus === 'off') {
+  this.streamingService.webButton.subscribe(r => {webStatus = r; });
+  this.streamingService.playButton.subscribe(r => {playStatus = r; });
+  this.streamingService.recButton.subscribe(r => {recStatus = r; });
+ if ( playStatus === 'off' && r2rStatus === 'off') {
    this.controlStatus = true;
-  this.r2rColor = 'primary';
+  this.r2rColor = 'accent';
   this.streamingService.r2rButton.next('standby');
- } else if (playStatus === 'on' && r2rStatus === 'standby') {
-  this.controlStatus = true;
-   this.streamingService.r2rButton.next('on');
-   this.r2rColor = 'warn';
- }  else if (playStatus === 'on' && r2rStatus === 'off') {
-  this.controlStatus = true;
-  this.streamingService.r2rButton.next('on');
-  this.r2rColor = 'warn';
- } else if (playStatus === 'on' && r2rStatus === 'standby') {
-  this.controlStatus = true;
-  this.streamingService.r2rButton.next('on');
-  this.r2rColor = 'warn';
- } else if (playStatus === 'on' && r2rStatus === 'on') {
-  this.controlStatus = true;
+  this.streamingService.playButton.next('on');
+ } else  if (playStatus === 'on' && r2rStatus === 'standby') {
+  this.r2rColor = 'basic';
+   this.streamingService.r2rButton.next('off');
+ } else  if (playStatus === 'on' && r2rStatus === 'off') {
+  this.r2rColor = 'accent';
   this.streamingService.r2rButton.next('standby');
-  this.r2rColor = 'primary';
+ } else  if (playStatus === 'rec' && r2rStatus === 'standby') {
+  this.r2rColor = 'warn';
+  this.streamingService.r2rButton.next('on');
+ } else  if (playStatus === 'rec' && r2rStatus === 'off') {
+  this.r2rColor = 'warn';
+  this.streamingService.r2rButton.next('on');
+ } else  if (playStatus === 'rec' && r2rStatus === 'on') {
+  this.r2rColor = 'accent';
+  this.streamingService.r2rButton.next('standby');
+ } 
+
+ if (recStatus === 'off' && r2rStatus === 'off' && webStatus === 'off') {
+  this.controlStatus = false;
+  this.streamingService.playButton.next('off');
 }
-}
+  }
+
 
 onPlayButtonPressed() {
   let playStatus;
@@ -140,22 +171,71 @@ onPlayButtonPressed() {
   this.streamingService.recButton.subscribe(r => {recStatus = r; });
   this.streamingService.webButton.subscribe(r => {webStatus = r; });
   this.streamingService.playButton.subscribe(r => {playStatus = r; });
- if ( playStatus === 'off' && webStatus === 'off' && recStatus === 'off' && r2rStatus === 'off' ) {
-  this.recColor = ' ';
+ if ( playStatus === 'on' && (webStatus === 'standby' || recStatus === 'standby' || r2rStatus === 'standby' ) ) {
+  this.playColor = '#f44336';
+  
+  this.streamingService.playButton.next('rec');
+  
+   if  (webStatus === 'standby') {
+     if (playStatus === 'rec' && webStatus === 'standby') {
+      this.webColor = 'warn';
+      this.streamingService.webButton.next('on');
+     } else  if (playStatus === 'rec' && webStatus === 'off') {
+      this.webColor = 'warn';
+      this.streamingService.webButton.next('on');
+     } else  if (playStatus === 'rec' && webStatus === 'on') {
+      this.webColor = 'accent';
+      this.streamingService.webButton.next('standby');
+     }} else if  (recStatus === 'standby') {
 
-  this.controlStatus = false;
- } else if (playStatus === 'off' && (webStatus === 'standby' || recStatus === 'standby' || r2rStatus === 'standby') ) {
-  this.controlStatus = true;
-  this.pausOn = true;
-   this.streamingService.playButton.next('on');
-   this.recColor = 'warn';
- } else if (playStatus === 'on' && (webStatus === 'standby' || recStatus === 'standby' || r2rStatus === 'standby') ) {
+       if (playStatus === 'rec' && recStatus === 'standby') {
+        this.recColor = 'warn';
+        this.streamingService.recButton.next('on');
+       } else  if (playStatus === 'rec' && recStatus === 'off') {
+        this.recColor = 'warn';
+        this.streamingService.recButton.next('on');
+       } else  if (playStatus === 'rec' && recStatus === 'on') {
+        this.recColor = 'accent';
+        this.streamingService.recButton.next('standby');
+       }} else if  (r2rStatus === 'standby') {
+        if (playStatus === 'rec' && r2rStatus === 'standby') {
+          this.r2rColor = 'warn';
+          this.streamingService.r2rButton.next('on');
+         } else  if (playStatus === 'rec' && r2rStatus === 'off') {
+          this.r2rColor = 'warn';
+          this.streamingService.r2rButton.next('on');
+         } else  if (playStatus === 'rec' && r2rStatus === 'on') {
+          this.r2rColor = 'accent';
+          this.streamingService.r2rButton.next('standby');
+         } }
+
+         else if (playStatus === 'rec' && (webStatus === 'on' || recStatus === 'on' || r2rStatus === 'on')) {
+
+  if (webStatus === 'on' && recStatus === 'on' && r2rStatus === 'on') {
+    this.onR2RButtonPressed(); this.onRecButtonPressed(); this.onWebButtonPressed();
+  } else if  (webStatus === 'on') {
+    this.onWebButtonPressed();
+  } else if  (recStatus === 'on') {
+    this.onRecButtonPressed();
+  } else if  (r2rStatus === 'on') {
+    this.onR2RButtonPressed();
+  this.playColor = 'purple';
   this.pausOn = false;
-  this.recColor = ' ';
-  this.pausOn = true;
+  this.streamingService.playButton.next('on');
+ }
+ }
+ }this.pausOn = true;}
+onStopButtonPressed() {
+  this.playColor = '#6B6B6B';
+  this.pausOn = false;
+  this.controlStatus = false;
+  this.recColor = 'purple';
+  this.r2rColor = 'purple';
+  this.webColor = 'purple';
+  this.streamingService.playButton.next('off');
+  this.streamingService.r2rButton.next('off');
+  this.streamingService.webButton.next('off');
+  this.streamingService.recButton.next('off');
 }
-
-}
-onStopButtonPressed(){}
 
 }
